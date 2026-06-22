09fc8ec83baa94a0a19b37438bbf6ee6265d06a9
NRAJKU <neerajrajputytbr@gmail.com>
2026-06-23 01:31:38 +0530

    Fix the k8s config

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,19 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+        "spring.flyway.enabled=false",
+        "DB_HOST=localhost",
+        "DB_PORT=5432",
+        "DB_NAME=customerdb",
+        "DB_USERNAME=appuser",
+        "DB_PASSWORD=password"
+    }
+)
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {
+  }
 }
